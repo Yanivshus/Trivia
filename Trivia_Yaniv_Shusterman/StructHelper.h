2ee0e3361b22f4e64b5605b4563e093a263d20e5
Yaniv Shusterman <yanivdev26@gmail.com>
2024-03-29 17:00:29 +0300

    created the files and the struct helper file

---
+++ Trivia_Yaniv_Shusterman/StructHelper.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+
+typedef struct {
+	int status;
+} ErrorResponse;
+
+typedef struct {
+	int status;
+} LoginResponse;
+
+typedef struct {
+	int status;
+} SignupResponse;
+
+
